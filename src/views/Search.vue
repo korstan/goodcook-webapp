33069b693fb5bd4467591bf2433d4473416aefbf
korstan <stas.korda@gmail.com>
2020-04-09 23:09:10 +0500

    raw hardcoded recipe cards

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,20 @@
 <template>
-  <h1>Search results!</h1>
+  <div>
+    <h1>Search results!</h1>
+    <RecipeCard />
+    <RecipeCard />
+    <RecipeCard />
+    <RecipeCard />
+  </div>
 </template>
 
 <script>
+import RecipeCard from '@/components/RecipeCard/RecipeCard.vue';
 import { mapActions } from 'vuex';
+
 export default {
   name: 'Search',
+  components: { RecipeCard },
   mounted() {
     this.SHOW_HEADER();
   },
