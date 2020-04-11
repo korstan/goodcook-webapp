3fb44ed9ab3858b13a0bfba1bb354b8527166c54
bup_qud <31836862+Lohogoy@users.noreply.github.com>
2020-04-12 02:55:21 +0500

    порефакторил эбаут

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,79 @@
 <template>
   <div class="about">
-    <h1>Команда мечты</h1>
-    <div class="contentBox">
-      <v-img class="image" contain src="@/images/ivan.jpg" alt="Ваня" />
-      <v-img class="image" contain src="@/images/dima.jpg" alt="Дима" />
-      <v-img class="image" contain src="@/images/stas.jpg" alt="Стас" />
-      <v-img class="image" contain src="@/images/ivanM.jpg" alt="Другой Ваня" />
-      <v-img class="image" contain src="@/images/misha.jpg" alt="Миша" />
-      <v-img class="image image_mem hide" contain src="@/images/ivan_mem.jpg" />
-      <v-img class="image image_mem hide" contain src="@/images/dima_mem.jpg" />
-      <v-img class="image image_mem hide" contain src="@/images/stas_mem.jpg" />
-      <v-img class="image image_mem hide" contain src="@/images/ivanM_mem.jpg" />
-      <v-img class="image image_mem hide" contain src="@/images/misha_mem.jpg" />
+    <h1 class="text-center m-t-120">Команда мечты</h1>
+    <div class="contentBox m-t-80">
+      <div class="contentBox__item">
+        <v-img class="image" contain src="@/images/ivan.jpg" alt="Ваня" />
+        <v-img
+          class="image image_mem hide"
+          contain
+          src="@/images/ivan_mem.jpg"
+        />
+        <div class="contentBox__item-name">Баширов Иван</div>
+        <div class="contentBox__item-desk text">
+          Создал дизайн проекта, помогал в&nbsp;разработке визуальной части
+          сервиса и рисовал иллюстрации
+        </div>
+      </div>
+      <div class="contentBox__item">
+        <v-img class="image" contain src="@/images/dima.jpg" alt="Дима" />
+        <v-img
+          class="image image_mem hide"
+          contain
+          src="@/images/dima_mem.jpg"
+        />
+
+        <div class="contentBox__item-name">Щапин Дмитрий</div>
+        <div class="contentBox__item-desk">
+          Предложил идею этого проекта, руководил разработкой, проводил встречи
+          команды
+        </div>
+      </div>
+      <div class="contentBox__item">
+        <v-img class="image" contain src="@/images/stas.jpg" alt="Стас" />
+        <v-img
+          class="image image_mem hide"
+          contain
+          src="@/images/stas_mem.jpg"
+        />
+
+        <div class="contentBox__item-name">Корда Станислав</div>
+        <div class="contentBox__item-desk">
+          Разрабатывал frontend сервиса, занимался логистикой страниц проекта
+          и&nbsp;создавал компоненты
+        </div>
+      </div>
+      <div class="contentBox__item">
+        <v-img
+          class="image"
+          contain
+          src="@/images/ivanM.jpg"
+          alt="Другой Ваня"
+        />
+        <v-img
+          class="image image_mem hide"
+          contain
+          src="@/images/ivanM_mem.jpg"
+        />
+        <div class="contentBox__item-name">Морозов Иван</div>
+        <div class="contentBox__item-desk">
+          Разрабатывал и&nbsp;наполнял информационную базу проекта, занимался
+          деплоем проекта
+        </div>
+      </div>
+      <div class="contentBox__item">
+        <v-img class="image" contain src="@/images/misha.jpg" alt="Миша" />
+        <v-imgу
+          class="image image_mem hide"
+          contain
+          src="@/images/misha_mem.jpg"
+        />
+        <div class="contentBox__item-name">Мишин Александр</div>
+        <div class="contentBox__item-desk">
+          Занимался тестированием сервиса, оформлением технического задания
+          и&nbsp;пояснительной записки
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -39,15 +101,33 @@ document.body.onclick = function() {
   justify-content: space-between;
   flex-wrap: wrap;
 
-  .image {
-    background: $gray;
-    border-radius: $default-border-radius;
-    max-width: 300px;
-    max-height: 300px;
-    display: flex;
-    padding: 10px;
+  &__item {
+    width: 300px;
+    height: 300px;
+    font-style: normal;
+    font-weight: normal;
     align-self: center;
-    &_mem {
+
+    .image {
+      display: flex;
+      background: white;
+      border: solid 8px hsla(0, 0%, 55%, 0.5);
+      border-radius: $default-border-radius;
+      max-width: 300px;
+      max-height: 300px;
+    }
+
+    &-name {
+      margin-top: 20px;
+      text-align: center;
+      font-size: 28px;
+      line-height: 56px;
+      letter-spacing: 0.25px;
+    }
+    &-desk {
+      font-size: 17px;
+      line-height: 42px;
+      opacity: 70%;
     }
   }
 }
