e0df8ed0cd0ce8325a9f626431015befd1d5af3f
korstan <stas.korda@gmail.com>
2020-04-12 05:12:38 +0500

    tryna fix deployment again

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const serveStatic = require('serve-static');
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 const port = process.env.PORT || 80;
 app.listen(port);
