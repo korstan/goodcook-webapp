9f4c891b6b9aa03f197a8707ba3cea94d769476f
korstan <stas.korda@gmail.com>
2020-04-14 14:24:46 +0500

    build, add fallback

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const fallback = require('express-history-api-fallback');
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
-});
-const port = process.env.PORT || 5000;
+app.use(fallback(__dirname + '/dist/index.html'));
+const port = process.env.PORT || 8002;
 app.listen(port);
